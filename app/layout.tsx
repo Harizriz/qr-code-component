466d6aa3ee1896f18739c65db65d77f25886a733
Hariz Mohd Hafiz Tay <hariztay.work@gmail.com>
2023-03-06 16:25:15 +0800

    added title attribute for html tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" title="QR Code Component">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
